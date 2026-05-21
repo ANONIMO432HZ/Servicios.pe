b83a7b1f0364de1c14878a867237aa5d8aea6d29
4N0N1M0 <208513646+ANONIMO432HZ@users.noreply.github.com>
2026-05-21 18:52:50 -0500

    fix: correct the .vercel directory exclusion pattern in .gitignore

--- dist/server/entry.mjs
+++
@@ -1,6 +0,0 @@
-export { h as handler, o as options, a as startServer } from './chunks/server_rB8itLAG.mjs';
-import './chunks/params-and-props_CvnwIJai.mjs';
-import 'piccolore';
-import 'es-module-lexer';
-import 'clsx';
-import 'html-escaper';
